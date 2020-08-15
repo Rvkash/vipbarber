5d50b6baec18ffe3e4922f0c4ca9a642f188b509
Rvkash <rafael.ieel@hotmail.com>
2020-08-15 00:35:50 -0300

    add: controllers config

---
+++ src/controllers/BarberController.js
@@ -0,0 +1,12 @@
+exports.inicio = function(req, res) {
+    return res.render('index')
+}
+
+exports.agendar = function (req, res) {
+    return res.render('agendar')
+}
+
+exports.cadastro = function (req, res) {
+    return res.render('cadastro')
+}
+
